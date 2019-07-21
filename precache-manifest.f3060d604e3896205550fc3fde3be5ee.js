49ee72b10a3448f6b5d95bc0a96009eb8601c17e
maryavasylieva <miheta.laurelin@gmail.com>
2019-07-21 21:00:16 +0300

    Updates

---
+++ precache-manifest.f3060d604e3896205550fc3fde3be5ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a5785373f22219628ad508c30398a0",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "2589b10e39238c9b7b16",
+    "url": "/goit-react-hw-02-reader/static/css/main.600a56d5.chunk.css"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/goit-react-hw-02-reader/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "2589b10e39238c9b7b16",
+    "url": "/goit-react-hw-02-reader/static/js/main.7e7715e5.chunk.js"
+  },
+  {
+    "revision": "0074b703c3097bd9a1db",
+    "url": "/goit-react-hw-02-reader/static/js/runtime~main.da19d381.js"
+  }
+]);
